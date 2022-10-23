ea7b795c08fc2d55d05d025a2162f7861fdfef81
Nelson Osvaldo Salinas Guajardo <ns4lin4s@gmail.com>
2022-10-22 22:16:39 -0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,50 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+echo "Hello $1 | $2 | $3 | $4 | $5"
+
+# Create a dedicated profile for this action to avoid conflicts
+# with past/future actions.
+aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+$4
+$5
+$6
+text
+EOF
 
 export IFS=","
 files_added="$1"
 for file in $files_added; do
-  echo "ADDED: $file"
+    echo "ADDED: $file"
+    # Sync using our dedicated profile and suppress verbose messages.
+    # All other flags are optional via the `args:` directive.
+    sh -c "aws s3 sync ./$file s3://$3/$file \
+                --profile s3-sync-action \
+                --no-progress \
+                ${ENDPOINT_APPEND} $*"
 done
 
 files_modified="$2"
 for file in $files_modified; do
-  echo "MODIFIED: $file"
+    echo "MODIFIED: $file"
+    # Sync using our dedicated profile and suppress verbose messages.
+    # All other flags are optional via the `args:` directive.
+    sh -c "aws s3 sync ./$files_modified s3://$3/$files_modified \
+                --profile s3-sync-action \
+                --no-progress \
+                ${ENDPOINT_APPEND} $*"
 done
 
-files_added_modified="$3"
-for file in $files_added_modified; do
-  echo "ADDED-MODIFIED: $file"
-done
+# Clear out credentials after we're done.
+# We need to re-run `aws configure` with bogus input instead of
+# deleting ~/.aws in case there are other credentials living there.
+# https://forums.aws.amazon.com/thread.jspa?threadID=148833
+aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+null
+null
+null
+text
+EOF
+
 
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT

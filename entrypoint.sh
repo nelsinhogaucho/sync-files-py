a35fef76742f6a035c5da772a944bdd467a068f0
Nelson Osvaldo Salinas Guajardo <ns4lin4s@gmail.com>
2022-10-22 21:28:24 -0300

    new step

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,19 @@
 echo "Hello $1"
 
 export IFS=","
-files_changed="$1"
-for file in $files_changed; do
-  echo "$file"
+files_added="$1"
+for file in $files_added; do
+  echo "ADDED: $file"
+done
+
+files_modified="$2"
+for file in $files_modified; do
+  echo "MODIFIED: $file"
+done
+
+files_added_modified="$3"
+for file in $files_added_modified; do
+  echo "ADDED-MODIFIED: $file"
 done
 
 time=$(date)

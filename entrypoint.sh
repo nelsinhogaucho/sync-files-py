9ef08212dd3d74be14e180f0301575d863c02340
Nelson Osvaldo Salinas Guajardo <ns4lin4s@gmail.com>
2022-10-24 20:55:54 -0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ files_added="$1"
 for file in $files_added; do
     echo "ADDED: $file"
 
-    cmd="aws s3 sync ./$file s3://$3/$file --profile s3-sync-action --no-progress --exclude *.sh --debug 2> debug.log"
+    cmd="aws s3 sync ./$file s3://$3/$file --profile s3-sync-action --no-progress --exclude *.sh"
     echo $cmd
     eval $cmd
 
@@ -29,7 +29,7 @@ files_modified="$2"
 for file in $files_modified; do
     echo "MODIFIED: $file"
 
-    cmd="aws s3 sync ./$file s3://$3/$file --profile s3-sync-action --no-progress --exclude *.sh --debug 2> debug.log"
+    cmd="aws s3 sync ./$file s3://$3/$file --profile s3-sync-action --no-progress --exclude *.sh"
     echo $cmd
     eval $cmd
 
